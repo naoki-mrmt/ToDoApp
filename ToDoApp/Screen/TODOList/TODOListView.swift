facca2645f81707011bf2ec9b0c7f925a9f49fa0
naoki-mrnk <20cm0135@jec.ac.jp>
2023-01-14 23:18:33 +0900

    add TODOListView

---
+++ ToDoApp/Screen/TODOList/TODOListView.swift
@@ -0,0 +1,20 @@
+//
+//  TODOListView.swift
+//  ToDoApp
+//
+//  Created by naoki-mrmt on 2023/01/14.
+//
+
+import SwiftUI
+
+struct TODOListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TODOListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TODOListView()
+    }
+}
