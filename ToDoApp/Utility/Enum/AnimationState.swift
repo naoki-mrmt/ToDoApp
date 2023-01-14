3f886485b2235016df4fe72378a3e232baaebdc0
naoki-mrnk <20cm0135@jec.ac.jp>
2023-01-14 23:18:18 +0900

    add animationState

---
+++ ToDoApp/Utility/Enum/AnimationState.swift
@@ -0,0 +1,14 @@
+//
+//  AnimationState.swift
+//  ToDoApp
+//
+//  Created by naoki-mrmt on 2023/01/14.
+//
+
+import Foundation
+
+enum AnimationState {
+    case normal
+    case compress
+    case expand
+}
